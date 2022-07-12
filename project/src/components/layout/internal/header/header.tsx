26114fd9d9f058006710aeebdabcca76d533a494
Igor Chizhov <igor209@mail.ru>
2022-07-12 11:15:51 +0400

    3.2. Add logo component

--- project/src/components/layout/internal/header/header.tsx
+++ project/src/components/layout/internal/header/header.tsx
@@ -1,4 +1,5 @@
 import HeaderNav from '../header-nav/header-nav';
+import Logo from '../../../logo/logo'
 
 type HeaderProps = {
   headerWithNav: boolean;
@@ -10,9 +11,7 @@ function Header({headerWithNav}: HeaderProps): JSX.Element {
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <a className="header__logo-link header__logo-link--active">
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-            </a>
+            <Logo />
           </div>
           {headerWithNav && <HeaderNav />}
         </div>
