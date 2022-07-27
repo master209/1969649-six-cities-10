0df7f9c0adb11bee232673d4edb2eb51b05d3df2
chizhov.in <chizhov.in@gals-telecom.ru>
2022-07-27 12:19:12 +0400

    6.2.8. Sorting numbers & strings fix

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -1,14 +1,13 @@
 export const sortToHigh = (array: any[], key: string): any[] =>
   array.sort(({[key]: a}, {[key]: b}) =>
-    isNaN(Number(a) - Number(b)) ? a.localeCompare(b) : Number(a) - Number(b)
+    isNaN(+a - +b) ? a.localeCompare(b) : +a - +b
   );
-
 
 export const sortToLow = (array: any[], key: string): any[] =>
   array.sort(({[key]: a}, {[key]: b}) =>
-    isNaN(Number(a) - Number(b)) ? b.localeCompare(a) : Number(b) - Number(a)
+    isNaN(+a - +b) ? b.localeCompare(a) : +b - +a
   );
 
 export const sortRated = (array: any[], key1: string, key2: string): any[] =>
   array.sort(({[key1]: a}, {[key1]: b}) =>
-    Number(b[key2]) - Number(a[key2]));
+    +(b[key2]) - +(a[key2]));
