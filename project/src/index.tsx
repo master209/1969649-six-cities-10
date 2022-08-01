92c51a87e8db6b5a5f6911a9391b34c72d2d0e6f
chizhov.in <chizhov.in@gals-telecom.ru>
2022-08-01 17:04:29 +0400

    7.3.2. Http errors notifier

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
+import {ToastContainer} from 'react-toastify';
 
 import {store} from './store';
 import {fetchOffersAction, checkAuthAction} from './store/api-actions';
 
 import App from './components/app/app';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 store.dispatch(fetchOffersAction());
 store.dispatch(checkAuthAction());
@@ -17,6 +20,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store = {store}>
+      <ToastContainer />
       <App />
     </Provider>
   </React.StrictMode>,
--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,6 +1,21 @@
-import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
+import {StatusCodes} from 'http-status-codes';
+import {toast} from 'react-toastify';
+import axios, {
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+  AxiosError
+} from 'axios';
 
 import {getToken} from './token';
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
 const BACKEND_URL = 'https://10.react.pages.academy/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -21,6 +36,17 @@ export const createAPI = (): AxiosInstance => {
 
       return config;
     },
+  );
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        toast.warn(error.response.data.error);
+      }
+
+      throw error;
+    }
   );
 
   return api;
