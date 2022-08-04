92ab69544941b71d9702e2a33f692039de37d351
chizhov.in <chizhov.in@gals-telecom.ru>
2022-08-04 17:30:54 +0400

    8.2.6. Favorites page - split by city

--- project/src/components/favorites-screen/favorites-list.tsx
+++ project/src/components/favorites-screen/favorites-list.tsx
@@ -5,35 +5,54 @@ import {getFavorites, getIsFavoritesLoaded} from '../../store/favorite-data/sele
 
 import {useAppSelector} from '../../hooks';
 
+import {Offer} from '../../types/offers';
+
+import {cities} from '../../const';
+
 /* «Список избранных предложений» */
 function FavoritesList(): JSX.Element {
   const favorites = useAppSelector(getFavorites);
   const isFavoritesLoaded = useAppSelector(getIsFavoritesLoaded);
 
-  return (
-    <div className="page">
-      {isFavoritesLoaded && favorites.length
-        ? (
-          <ul className="favorites__list">
-            <li className="favorites__locations-items">
+  // вычисляет количество favorites в городе city
+  const favoritesByCityCount = (city: string): number =>
+    favorites.reduce((acc, item: Offer) =>
+      item.city.name === city ? ++acc : 0, 0);
+
+  const renderFavorites = () => (
+    <ul className="favorites__list">
+      {cities.map((city) =>
+        favoritesByCityCount(city)
+          ?
+          (
+            <li key={city} className="favorites__locations-items">
               <div className="favorites__locations locations locations--current">
                 <div className="locations__item">
                   <a className="locations__item-link" href="#todo">
-                    <span>Amsterdam</span>
+                    <span>{city}</span>
                   </a>
                 </div>
               </div>
               <div className="favorites__places">
                 {favorites.map((favorite) => (
-                  <FavoritesCard
-                    key={favorite.id}
-                    offer={favorite}
-                  />
+                  favorite.city.name === city && (
+                    <FavoritesCard
+                      key={favorite.id}
+                      offer={favorite}
+                    />)
                 ))}
               </div>
             </li>
-          </ul>
-        )
+          )
+          : null
+      )}
+    </ul>
+  );
+
+  return (
+    <div className="page">
+      {isFavoritesLoaded && favorites.length
+        ? renderFavorites()
         : <FavoritesEmptyScreen />}
     </div>
   );
