8e69ecffac87ada12049c1a50b2cdd9d4aff5688
Igor Chizhov <igor209@mail.ru>
2022-08-11 00:04:39 +0400

    9.1.12. Reducer userProcess tests

---
+++ project/src/store/user-process/user-process.test.ts
@@ -0,0 +1,46 @@
+import {userProcess} from './user-process';
+import {UserProcess} from '../../types/state';
+import {AuthorizationStatus} from '../../const';
+import {fetchCheckAuth, fetchLogin, fetchLogout} from '../api-actions';
+
+describe('Reducer: userProcess', () => {
+  let state: UserProcess;
+
+  beforeEach(() => {
+    state = {authorizationStatus: AuthorizationStatus.Unknown};
+  });
+
+  it('without additional parameters should return initial state', () => {
+    expect(userProcess.reducer(undefined, {type: 'UNKNOWN_ACTION'}))
+      .toEqual({authorizationStatus: AuthorizationStatus.Unknown});
+  });
+
+  describe('fetchCheckAuth test', () => {
+    it('should update authorizationStatus to "AUTH" if fetchCheckAuth fulfilled', () => {
+      expect(userProcess.reducer(state, { type: fetchCheckAuth.fulfilled.type }))
+        .toEqual({authorizationStatus: AuthorizationStatus.Auth});
+    });
+    it('should update authorizationStatus to "NO_AUTH" if fetchCheckAuth rejected', () => {
+      expect(userProcess.reducer(state, { type: fetchCheckAuth.rejected.type }))
+        .toEqual({authorizationStatus: AuthorizationStatus.NoAuth});
+    });
+  });
+
+  describe('fetchLogin test', () => {
+    it('should update authorizationStatus to "AUTH" if fetchLogin fulfilled', () => {
+      expect(userProcess.reducer(state, { type: fetchLogin.fulfilled.type }))
+        .toEqual({authorizationStatus: AuthorizationStatus.Auth});
+    });
+    it('should update authorizationStatus to "NO_AUTH" if fetchLogin rejected', () => {
+      expect(userProcess.reducer(state, { type: fetchLogin.rejected.type }))
+        .toEqual({authorizationStatus: AuthorizationStatus.NoAuth});
+    });
+  });
+
+  describe('fetchLogout test', () => {
+    it('should update authorizationStatus to "NO_AUTH" if fetchLogout fulfilled', () => {
+      expect(userProcess.reducer(state, { type: fetchLogout.fulfilled.type }))
+        .toEqual({authorizationStatus: AuthorizationStatus.NoAuth});
+    });
+  });
+});
