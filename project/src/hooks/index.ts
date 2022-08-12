962094d0287ec46575e244792aaad493cb017fc2
chizhov.in <chizhov.in@gals-telecom.ru>
2022-08-12 12:23:36 +0400

    9.2.10. Hook useIsAuthorized test

--- project/src/hooks/index.ts
+++ project/src/hooks/index.ts
@@ -1,6 +1,6 @@
 import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 
-import type {State, AppDispatch} from '../types/state';
+import {State, AppDispatch} from '../types/state';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
---
+++ project/src/hooks/is-authorized.test.ts
@@ -0,0 +1,18 @@
+import useIsAuthorized from './is-authorized';
+
+jest.mock('./app-selectors', () => {
+  const mockUseAppSelectors = () => ({authorizationStatus: 'AUTH'});
+
+  return {
+    __esModule: true,
+    default: mockUseAppSelectors,
+  };
+});
+
+describe('Hook: useIsAuthorized', () => {
+  it('should return "true" when user authorized', () => {
+    const isAuthorized = useIsAuthorized();
+
+    expect(isAuthorized).toBe(true);
+  });
+});
