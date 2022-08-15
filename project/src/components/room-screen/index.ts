729ac331724124c8ca3accf3e2542193a7022db3
Igor Chizhov <igor209@mail.ru>
2022-08-16 00:46:23 +0400

    9.3.10. RoomScreen component - Map

--- project/src/components/room-screen/index.ts
+++ project/src/components/room-screen/index.ts
@@ -1,5 +1,5 @@
 export {default as OfferInsideItem} from './features-inside';
 export {default as OfferNearsList} from './offer-nears-list';
 export {CommentForm, CommentsList, CommentItem} from './comment';
-export {Gallery, Property, Host} from './internal';
+export {Gallery, Property, Host, Reviews} from './internal';
 
--- project/src/pages/room-screen/room-screen.tsx
+++ project/src/pages/room-screen/room-screen.tsx
@@ -4,26 +4,21 @@ import {useParams, useNavigate} from 'react-router-dom';
 import Layout from '../../components/layout/layout';
 import {Loader} from '../../components/common';
 import {
-  CommentForm,
-  CommentsList,
   OfferNearsList,
   Gallery,
   Property,
   Host,
+  Reviews
 } from '../../components/room-screen';
 
-import {
-  fetchLoadComments,
-  fetchOffer,
-  fetchLoadOffersNear
-} from '../../store/api-actions';
+import {fetchLoadComments, fetchOffer, fetchLoadOffersNear} from '../../store/api-actions';
 
 import {useAppDispatch} from '../../hooks';
-import useSetOffersFavoriteStatus from '../../hooks/set-offers-favorite-status';
-import useIsAuthorized from '../../hooks/is-authorized';
 import useAppSelectors from '../../hooks/app-selectors';
+import useIsAuthorized from '../../hooks/is-authorized';
+import useSetOffersFavoriteStatus from '../../hooks/set-offers-favorite-status';
 
-import {Offer, City, Location, Locations} from '../../types/offers';
+import {City, Location, Locations} from '../../types/offers';
 
 import {AppRoute} from '../../const';
 
@@ -37,14 +32,15 @@ type RoomProps = {
 };
 
 function RoomScreen({renderMap}: RoomProps): JSX.Element {
+  const {offers, isOfferLoading, offer, offersNear, comments, isError404} = useAppSelectors();
+
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const isAuthorized = useIsAuthorized();
 
+  isError404 && navigate(AppRoute.NotFound);
+
   useSetOffersFavoriteStatus(true);
-
-  const {offers, isOfferLoading, offer, offersNear, comments, isError404} = useAppSelectors();
-
 
   const {id} = useParams();
 
@@ -56,51 +52,37 @@ function RoomScreen({renderMap}: RoomProps): JSX.Element {
     }
   },[id]);
 
-  isError404 && navigate(AppRoute.NotFound);
+  const locations = offersNear.map((_offer) => _offer.location);
 
-  const locations = offersNear.map((offerNear) => offerNear.location);
-
-  const renderOffer = (_offer: Offer) => {
-
-    const selectedLocation = _offer.location;
-    locations.push(selectedLocation);
-
-    return (
-      <Layout>
-        <main className="page__main page__main--property">
-          <section className="property">
-            <Gallery offer={_offer}/>
-            <div className="property__container container">
-              <div className="property__wrapper">
-                <Property offer={_offer} isAuthorized={isAuthorized} />
-                <Host offer={_offer}/>
-
-                <section className="property__reviews reviews">
-                  <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{comments.length}</span></h2>
-                  <CommentsList
-                    comments={comments}
-                  />
-                  {isAuthorized && offer && <CommentForm offerId={offer.id} />}
-                </section>
-
-              </div>
-            </div>
-
-            {renderMap(offers[0].city, locations, selectedLocation, 'property__map')}
-          </section>
-
-          <div className="container">
-            <OfferNearsList offersNear={offersNear} />
-          </div>
-        </main>
-      </Layout>
-    );
-  };
+  useEffect((): void => {
+    offer && locations.push(offer.location);
+  },[locations]);
 
   return (
     <div className="page">
       {offer && !isOfferLoading
-        ? renderOffer(offer)
+        ?
+        (
+          <Layout>
+            <main className="page__main page__main--property">
+              <section className="property">
+                <Gallery offer={offer}/>
+                <div className="property__container container">
+                  <div className="property__wrapper">
+                    <Property offer={offer} isAuthorized={isAuthorized} />
+                    <Host offer={offer}/>
+                    <Reviews offer={offer} comments={comments} isAuthorized={isAuthorized} />
+                  </div>
+                </div>
+                {renderMap(offers[0].city, locations, offer.location, 'property__map')}
+              </section>
+
+              <div className="container">
+                <OfferNearsList offersNear={offersNear} />
+              </div>
+            </main>
+          </Layout>
+        )
         : <Loader />}
     </div>
   );
